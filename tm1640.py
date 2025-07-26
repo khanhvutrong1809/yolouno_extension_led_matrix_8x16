89e2278568d4ea2748adc6b5d122160d8609425e
khanhvutrong1809 <khanh.vutrong1809@hcmut.edu.vn>
2025-07-26 21:24:57 +0700

    9h24

--- tm1640.py
+++ tm1640.py
@@ -1,84 +1,74 @@
-# tm1640_matrix.py
 from micropython import const
 from machine import Pin
 from time import sleep_us
 
-TM1640_CMD1 = const(0x40)  # Data command
-TM1640_CMD2 = const(0xC0)  # Address command
-TM1640_CMD3 = const(0x80)  # Display control command
-TM1640_DSP_ON = const(0x08)
-TM1640_DELAY = const(10)   # microseconds delay
-TM1640_CMD1 = const(64)   # 0x40: data command
-TM1640_CMD2 = const(192)  # 0xC0: address command
-TM1640_CMD3 = const(128)  # 0x80: display control command
-TM1640_DSP_ON = const(8)  # 0x08: display on
-TM1640_DELAY = const(10)  # 10us delay
+TM1640_CMD1 = const(64)  # 0x40 data command
+TM1640_CMD2 = const(192) # 0xC0 address command
+TM1640_CMD3 = const(128) # 0x80 display control command
+TM1640_DSP_ON = const(8) # 0x08 display on
+TM1640_DELAY = const(10) # 10us delay between clk/dio pulses
 
-class TM1640:
 class TM1640(object):
     """Library for LED matrix display modules based on the TM1640 LED driver."""
-def __init__(self, clk, dio, brightness=7):
-self.clk = clk
-self.dio = dio
-        self.buffer = bytearray(16)  # 16 columns (8x16 matrix)
+    def __init__(self, clk, dio, brightness=7):
+        self.clk = clk
+        self.dio = dio
 
-if not 0 <= brightness <= 7:
-raise ValueError("Brightness out of range")
-self._brightness = brightness
+        if not 0 <= brightness <= 7:
+            raise ValueError("Brightness out of range")
+        self._brightness = brightness
 
-self.clk.init(Pin.OUT, value=0)
-self.dio.init(Pin.OUT, value=0)
-sleep_us(TM1640_DELAY)
-
-self._write_data_cmd()
-self._write_dsp_ctrl()
-
-def _start(self):
-        self.dio(1)
-        self.clk(1)
+        self.clk.init(Pin.OUT, value=0)
+        self.dio.init(Pin.OUT, value=0)
         sleep_us(TM1640_DELAY)
-self.dio(0)
-sleep_us(TM1640_DELAY)
-self.clk(0)
-sleep_us(TM1640_DELAY)
 
-def _stop(self):
+        self._write_data_cmd()
+        self._write_dsp_ctrl()
+
+    def _start(self):
+        self.dio(0)
+        sleep_us(TM1640_DELAY)
         self.clk(0)
         sleep_us(TM1640_DELAY)
-self.dio(0)
-sleep_us(TM1640_DELAY)
-self.clk(1)
-sleep_us(TM1640_DELAY)
-self.dio(1)
+
+    def _stop(self):
+        self.dio(0)
         sleep_us(TM1640_DELAY)
+        self.clk(1)
+        sleep_us(TM1640_DELAY)
+        self.dio(1)
 
-def _write_data_cmd(self):
-self._start()
-self._write_byte(TM1640_CMD1)
-self._stop()
+    def _write_data_cmd(self):
+        # automatic address increment, normal mode
+        self._start()
+        self._write_byte(TM1640_CMD1)
+        self._stop()
 
-def _write_dsp_ctrl(self):
-self._start()
-self._write_byte(TM1640_CMD3 | TM1640_DSP_ON | self._brightness)
-self._stop()
+    def _write_dsp_ctrl(self):
+        # display on, set brightness
+        self._start()
+        self._write_byte(TM1640_CMD3 | TM1640_DSP_ON | self._brightness)
+        self._stop()
 
-def _write_byte(self, b):
-for i in range(8):
-self.dio((b >> i) & 1)
-sleep_us(TM1640_DELAY)
-self.clk(1)
-sleep_us(TM1640_DELAY)
-self.clk(0)
-sleep_us(TM1640_DELAY)
+    def _write_byte(self, b):
+        for i in range(8):
+            self.dio((b >> i) & 1)
+            sleep_us(TM1640_DELAY)
+            self.clk(1)
+            sleep_us(TM1640_DELAY)
+            self.clk(0)
+            sleep_us(TM1640_DELAY)
 
-def brightness(self, val=None):
+    def brightness(self, val=None):
         """Set the display brightness 0-7."""
-if val is None:
-return self._brightness
-if not 0 <= val <= 7:
-raise ValueError("Brightness out of range")
+        # brightness 0 = 1/16th pulse width
+        # brightness 7 = 14/16th pulse width
+        if val is None:
+            return self._brightness
+        if not 0 <= val <= 7:
+            raise ValueError("Brightness out of range")
 
-self._brightness = val
+        self._brightness = val
         self._write_data_cmd()
         self._write_dsp_ctrl()
 
@@ -86,79 +76,40 @@ self._brightness = val
         self.write(int.to_bytes(len, 'big'), pos)
 
     def write_hmsb(self, buf, pos=0):
+        # Tạo bản sao của buffer để chỉnh sửa
+        modified_buf = list(buf)
+        
+        # Hoán đổi cột 7 và cột 8 nếu chúng nằm trong phạm vi buffer
+        if len(modified_buf) > 7:  # Đảm bảo có đủ cột
+            modified_buf[7], modified_buf[6] = modified_buf[6], modified_buf[7]
+
         self._write_data_cmd()
         self._start()
 
         self._write_byte(TM1640_CMD2 | pos)
-        for i in range(7 - pos, -1, -1):
-            self._write_byte(buf[i])
+        for i in range(7-pos, -1, -1):
+            self._write_byte(modified_buf[i])
 
         self._stop()
-self._write_dsp_ctrl()
+        self._write_dsp_ctrl()
 
     def write(self, rows, pos=0):
         if not 0 <= pos <= 15:
             raise ValueError("Position out of range")
 
-        # Hoán đổi cột 7 và 8 nếu đủ 8 byte
-        data = list(rows)
-        if len(data) >= 8:
-            data[6], data[7] = data[7], data[6]
+        # Tạo bản sao của buffer để chỉnh sửa
+        modified_rows = list(rows)
+        
+        # Hoán đổi cột 7 và cột 8 nếu chúng nằm trong phạm vi buffer
+        if len(modified_rows) > 7:  # Đảm bảo có đủ cột
+            modified_rows[7], modified_rows[6] = modified_rows[6], modified_rows[7]
 
         self._write_data_cmd()
         self._start()
 
         self._write_byte(TM1640_CMD2 | pos)
-        for row in data:
+        for row in modified_rows:
             self._write_byte(row)
 
         self._stop()
         self._write_dsp_ctrl()
-
-
-def clear(self):
-self.buffer = bytearray(16)
-self.show()
-
-def show(self):
-self._write_data_cmd()
-self._start()
-self._write_byte(TM1640_CMD2 | 0x00)
-for b in self.buffer:
-self._write_byte(b)
-self._stop()
-self._write_dsp_ctrl()
-
-def set_pixel(self, x, y, val):
-if not (0 <= x < 16 and 0 <= y < 8):
-return
-if val:
-self.buffer[x] |= (1 << y)
-else:
-self.buffer[x] &= ~(1 << y)
-
-def draw_bitmap(self, data):
-for i in range(min(16, len(data))):
-self.buffer[i] = data[i]
-self.show()
-
-def scroll_left(self):
-for i in range(15):
-self.buffer[i] = self.buffer[i + 1]
-self.buffer[15] = 0
-self.show()
-
-def scroll_right(self):
-for i in range(15, 0, -1):
-self.buffer[i] = self.buffer[i - 1]
-self.buffer[0] = 0
-self.show()
-
-def draw_char(self, char, font, pos=0):
-if char not in font:
-return
-data = font[char]
-for i in range(min(8, len(data))):
-if pos + i < 16:
-self.buffer[pos + i] = data[i]
-self.show()
